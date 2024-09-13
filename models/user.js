a2a189f24e6ab864125e92802c6ec0b9289e25c2
PerScholarSarah <rmandolin22@gmail.com>
2024-09-13 14:38:04 -0400

    added accountID and email to userSchema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose");
+const validator = require("email-validator");
 
 const userSchema = new mongoose.Schema({
+    accountID : {
+        type: Number,
+        index: true,
+        required: true,
+        unique: true,
+        min: 0,
+    },
+    email : {
+        type: String,
+        unique: true,
+        validate: validator.validate
+    },
     username : {
         type: String, 
+        index: true,
+        unique: true,
         required: true,
         minLength: 3,
         maxLength: 32
