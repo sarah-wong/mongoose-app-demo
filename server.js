a46c2364b97867b1ce9335c1ca79054bf2d4878e
PerScholarSarah <rmandolin22@gmail.com>
2024-09-17 08:40:28 -0400

    final

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectToDB = require("./config/connectToDB");
+const userRouter = require("./routes/userRoutes");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 // SETUP
 app.use(express.json());
@@ -13,8 +14,10 @@ connectToDB();
 app.get("/", (req, res)=>{
     res.send("Hello World!");
 })
+app.use('/user', userRouter)
 
 // LISTEN
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on Port: ${PORT}`);
 })
